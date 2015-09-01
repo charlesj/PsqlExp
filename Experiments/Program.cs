4158b34a9de8c58bf8b221a27c108fac87e46b4e
Josh Charles <josh.charles@gmail.com>
2015-08-31 17:04:47 -0700

    Initial Commit

---
+++ Experiments/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Experiments
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello");
+#if DEBUG
+			Console.ReadLine();
+#endif
+		}
+	}
+}
